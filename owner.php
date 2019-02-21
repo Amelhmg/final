325e02a5d87fc8fde6fce0cc7b319736acd72131
Amelhmg <46347807+Amelhmg@users.noreply.github.com>
2019-02-21 19:08:36 +0100

    Add files via upload

---
+++ owner.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Travel Agency</title>
+</head>
+<body>
+    <div class="maindiv">
+        <header class="header">
+                    <a class="logo" href="index.php"> > Travel Agency < </a>
+        </header>
+
+        <div class="main">
+            <div class="inputbox">
+              <form method="POST" action="#####.js">
+               <div class="line"><p class="libelle">Nom :</p><input class="owninp" type="text" name="Nom"></div>
+               <div class="line"><p class="libelle">Prenom :</p><input class="owninp" type="text" name="Prenom"></div>
+               <div class="line"><p class="libelle">Pays :</p><input class="owninp" type="text" name="Pays"></div>
+               <div class="line"><p class="libelle">Adresse :</p><input class="owninp" type="text" name="Adresse"></div>
+               <div class="line"><p class="libelle">Hotel :</p><input class="owninp" type="text" name="Hotel"></div>
+               <div class="line"><p class="libelle">n° Chambres :</p><input class="owninp" type="text" name="n° Chambres"></div>
+               <div class="line"><p class="libelle">E-mail :</p><input class="owninp" type="text" name="E-mail"></div>
+               <button class="searchbutton">Soumettre ></button>
+            </div>
+              </form>
+        </div>
+
+        <footer class="footer">
+            <div class="contactinformation">
+                    <h4 class="h4contactinformation">Contact Information</h4>
+                    <p class="info">Contact : contact@travelagency.com</p>
+                    <p class="info">Tel : +21300000000</p>
+                    <p class="info">Fax : +21300000000</p>
+            </div>
+
+            <div class="copyrights">
+                <p>Travel Agency inc. Tout droit réservé</p>
+            </div>
+            <div class="follow">
+                <h4 class="followus">Nous suivre</h4>
+                <a class="followlinks" href="https://www.facebook.com/" target="_blank">Facebook</a>
+                <a class="followlinks" href="https://www.instagram.com/" target="_blank">Instagram</a>
+                <a class="followlinks" href="https://www.twitter.com/" target="_blank">Twitter</a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
